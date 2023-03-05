e2639e0476cf8ebd04d05e095beb97a52152f3b0
qlyzwd <q728@foxmail.com>
2023-03-05 22:35:11 +0800

    change

--- learn/c++/1.cpp
+++ learn/c++/1.cpp
@@ -3,7 +3,24 @@
 using namespace std;
 int main()
 {
-    cout<<"sb"<<endl;
+    //¹ş¹ş¹ş
+    int a,b,c;
+    cin>>a>>b>>c;
+    int d=b*b-4*a*c;
+    if(d<0)
+    {
+        cout<<"No real root"<<endl;
+    }
+    else if(d==0)
+    {
+        cout<<"Two same roots x="<<-b/(2*a)<<endl;
+    }
+    else
+    {
+        cout<<"Two different roots x1="<<(-b+sqrt(d))/(2*a)<<" , x2="<<(-b-sqrt(d))/(2*a)<<endl;
+    }
+    system("pause");
+
     
 }
     
