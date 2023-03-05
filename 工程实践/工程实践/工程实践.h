1e5e7ac33e626dcbfbc4256b3e22795f62ba7c68
qlyzwd <q728@foxmail.com>
2023-03-05 15:05:03 +0800

    unkown change

---
+++ 工程实践/工程实践/工程实践.h
@@ -0,0 +1,33 @@
+﻿
+// 工程实践.h: 工程实践 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// C工程实践App:
+// 有关此类的实现，请参阅 工程实践.cpp
+//
+
+class C工程实践App : public CWinApp
+{
+public:
+	C工程实践App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern C工程实践App theApp;
---
+++ 工程实践/工程实践/工程实践View.h
@@ -0,0 +1,49 @@
+﻿
+// 工程实践View.h: C工程实践View 类的接口
+//
+
+#pragma once
+
+
+class C工程实践View : public CView
+{
+protected: // 仅从序列化创建
+	C工程实践View() noexcept;
+	DECLARE_DYNCREATE(C工程实践View)
+
+// 特性
+public:
+	C工程实践Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~C工程实践View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // 工程实践View.cpp 中的调试版本
+inline C工程实践Doc* C工程实践View::GetDocument() const
+   { return reinterpret_cast<C工程实践Doc*>(m_pDocument); }
+#endif
+
