82be332e03583441b21aaf99132026c097d291c6
QLYZWD <q728@foxmail.com>
2023-03-05 16:27:23 +0800

    nothiing

--- learn/calss/123.c
+++ learn/calss/123.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<time.h>
+#include<ctype.h>
+#include<assert.h>
+
 int main()
 {
-    int *a;
-    printf("%p",a);
-    scanf("%d",&a);
-    printf("%",*a);
+    
+    //展示指针的用法
+    
+
+
     system("pause");
 }
